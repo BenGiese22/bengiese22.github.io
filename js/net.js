cededc1eef2faba3d035b8160853540dd296585c
Ben Giese <gieseba@msoe.edu>
2019-02-01 15:33:23 -0600

    Update net.js

--- js/net.js
+++ js/net.js
@@ -2,17 +2,27 @@ var tmi = require('tmi.js');
 
 // Define configuration options
 var opts = {
+    options: {
+        debug: true
+    },
+    connection: {
+        reconnect: true
+    },
     identity: {
         username: "Leveled22",
         password: "oauth:qj26e9v99aulexeswqh681umrg1uli",
     },
-    channels: [
-        "kickinkinger"
-    ]
+    channels: ['kickinkinger']
 };
 
 var client = new tmi.client(opts);
 
 client.connect();
 
-client.say("kickinkinger","Hello world");
+client.on("chat", (channel, user, message, self) => {
+    if(self) return
+})
+
+function onInputEnter() {
+    client.say('kickinkinger','hello world');
+}
